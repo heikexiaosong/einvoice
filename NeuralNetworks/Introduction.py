289b753b6d79e06c1b37a7850536d862b53edacb
songqi <heikexiaosong@gmail.com>
2022-09-22 23:30:50 +0800

    两个神经元的神经网络

--- NeuralNetworks/Introduction.py
+++ NeuralNetworks/Introduction.py
@@ -2,33 +2,48 @@
 
 import numpy as np
 
-
 def sigmoid(x):
-    # activation function: f(x) = 1 / (1 + e^(-x))
-    return 1 / (1 + np.exp(-x))
-
+  # Our activation function: f(x) = 1 / (1 + e^(-x))
+  return 1 / (1 + np.exp(-x))
 
 class Neuron:
-    def __init__(self, weights, bias):
-        self.weights = weights
-        self.bias = bias
+  def __init__(self, weights, bias):
+    self.weights = weights
+    self.bias = bias
 
-    def feedforward(self, inputs):
-        # Weight inputs, add bias, then use the activation function
-        total = np.dot(self.weights, inputs) + self.bias
-        return sigmoid(total)
+  def feedforward(self, inputs):
+    # Weight inputs, add bias, then use the activation function
+    total = np.dot(self.weights, inputs) + self.bias
+    return sigmoid(total)
 
+class OurNeuralNetwork:
+  '''
+  A neural network with:
+    - 2 inputs
+    - a hidden layer with 2 neurons (h1, h2)
+    - an output layer with 1 neuron (o1)
+  Each neuron has the same weights and bias:
+    - w = [0, 1]
+    - b = 0
+  '''
+  def __init__(self):
+    weights = np.array([0, 1])
+    bias = 0
 
-if __name__ == '__main__':
-    print("An Introduction to Neural Networks!")
+    # The Neuron class here is from the previous section
+    self.h1 = Neuron(weights, bias)
+    self.h2 = Neuron(weights, bias)
+    self.o1 = Neuron(weights, bias)
 
-    weights = np.array([0, 1])  # w1 = 0, w2 = 1
-    bias = 0  # b = 4
-    n = Neuron(weights, bias)
+  def feedforward(self, x):
+    out_h1 = self.h1.feedforward(x)
+    out_h2 = self.h2.feedforward(x)
 
-    x = np.array([2, 3])
-    x1 = n.feedforward(x)
-    print(x1)
+    # The inputs for o1 are the outputs from h1 and h2
+    out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
 
-    x = np.array([x1, x1])
-    print(n.feedforward(x))
+    return out_o1
+
+network = OurNeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x)) # 0.7216325609518421
